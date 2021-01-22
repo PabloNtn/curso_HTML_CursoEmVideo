6d152e87dd4b8ba1b2e03c6bf09208fb72bbe25e
Pablo <pablo.ntn2020@gmail.com>
2021-01-22 08:47:18 -0300

    Primera_atualizacao

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,10 @@
+function entrar(foto){
+    var img = document.getElementById('icone')
+    img.src = `../projeto-glass-html5/_imagens/${foto}.PNG`
+    
+} 
+
+function saida(foto){
+    var img = document.getElementById('icone')
+    img.src = `../projeto-glass-html5/_imagens/${foto}.png`  
+}
